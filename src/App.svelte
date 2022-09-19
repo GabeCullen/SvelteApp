4625f36d2d3c29433cd0ff96302c29822b76a138
Gabe <gcullen97@gmail.com>
2022-09-19 18:14:34 +1000

    Set up svelte app using typescript

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+import HomeDashboard from "./HomeDashboard.svelte";
+
+</script>
+
+<HomeDashboard />
---
+++ src/HomeDashboard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+   
+</script>
+
+<div>
+
+</div>
+
+<style>
+div{
+    height:100%;
+    width:100%;
+    background-color: rgb(0, 17, 37);
+}
+</style>
